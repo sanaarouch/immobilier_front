2186434dde3e6cd1e4b33f44d98452b0595a1cbe
Sana Aarouch <59958945+sanaarouch@users.noreply.github.com>
2025-09-22 12:01:29 +0200

    Correction de la parenthèse en trop

--- src/components/inscription/inscription.js
+++ src/components/inscription/inscription.js
@@ -57,7 +57,6 @@ const Inscription = () => {
         <h3>S'inscrire</h3>
         
         {error && <Alert variant="danger">{error}</Alert>}
-        }
         
         <div className={styles.formGroup}>
           <FormLabel>Nom</FormLabel>
